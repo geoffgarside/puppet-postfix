a9986c3f399662393da137805e539275e34b26c4
Geoff Garside <geoff@geoffgarside.co.uk>
2014-03-18 15:27:40 +0000

    Add postfix::transport backed by postfix::hash resources.

---
+++ manifests/hash.pp
@@ -0,0 +1,52 @@
+define postfix::hash (
+  $ensure   = 'present',
+  $file     = $title,
+  $source   = undef,
+  $content  = undef,
+) {
+  
+  require postfix
+
+  if $source and $content {
+    fail('You must provide either source OR content, NOT both')
+  }
+
+  $_file     = regsubst($file, '^\$config_directory', $::postfix::config_directory)
+  $_dbfile   = "${_file}.db"
+  $_postmap  = "postmap ${_file}"
+
+  $_file_ensure = $ensure ? {
+    'absent' => 'absent',
+    default  => 'file'
+  }
+
+  File {
+    mode  => '0600',
+    owner => '0',
+    group => '0',
+  }
+
+  file { $_file:
+    ensure  => $_file_ensure,
+    source  => $source,
+    content => $content,
+  }
+
+  file { $_dbfile:
+    ensure  => $_file_ensure,
+    require => [
+      File[$_file],
+      Exec[$_postmap]
+    ]
+  }
+
+  exec { $_postmap:
+    subscribe   => File[$_file],
+    refreshonly => true,
+    path        => [
+      '/sbin',            '/bin',
+      '/usr/sbin',        '/usr/bin',
+      '/usr/local/sbin',  '/usr/local/bin'
+    ],
+  }
+}
---
+++ manifests/transport.pp
@@ -0,0 +1,57 @@
+define postfix::transport (
+  $pattern    = $title,
+  $transport  = undef,
+  $nexthop    = undef,
+  $ensure     = 'present',
+  $file       = '$config_directory/transport'
+) {
+
+  $_file  = regsubst($file, '^\$config_directory', $::postfix::config_directory)
+  $_scope = "pattern[. = '${pattern}']"
+
+  if ! defined(Postfix::Hash[$file]) {
+    postfix::hash { $file:
+      ensure  => $ensure,
+    }
+  }
+  
+  if ! defined(Postfix::Conf['transport_maps']) {
+    postfix::conf { 'transport_maps':
+      ensure  => $ensure,
+      value   => $file,
+    }
+  }
+
+  case $ensure {
+    'present': {
+      if $transport {
+        $chg_transport = "set ${_scope}/transport '${transport}'"
+      } else {
+        $chg_transport = "clear ${_scope}/transport"
+      }
+
+      if $nexthop {
+        $chg_nexthop = "set ${_scope}/nexthop '${nexthop}'"
+      } else {
+        $chg_nexthop = "clear ${_scope}/nexthop"
+      }
+
+      $changes = [
+        "set ${_scope} '${pattern}'",
+        $chg_transport,
+        $chg_nexthop,
+      ]
+    }
+    'asbent': {
+      $changes = "rm ${_scope}"
+    }
+  }
+
+  augeas { "postfix/transport: ${name}":
+    incl    => $_file,
+    lens    => 'Postfix_Transport.lns',
+    changes => $changes,
+    require => Postfix::Hash[$file],
+    notify  => Postfix::Hash[$file],
+  }
+}
