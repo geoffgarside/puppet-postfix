69a89c98372c1d72f30179c59203832d9f437d2b
Geoff Garside <geoff@geoffgarside.co.uk>
2014-11-06 11:25:19 +0000

    Add new configuration variables
    These might later be removed and replaced by calling code simply using the postfix::conf resource type to define all of these settings.

--- manifests/params.pp
+++ manifests/params.pp
@@ -29,6 +29,23 @@ class postfix::params {
   $smtpd_soft_error_limit          = 10
   $smtpd_hard_error_limit          = 20
 
+  $smtp_tls_security_level         = 'may'
+  $smtp_tls_cert_file              = undef
+  $smtp_tls_key_file               = undef
+  $smtp_tls_loglevel               = 0
+
+  $smtpd_tls_auth_only             = false
+  $smtpd_tls_security_level        = may
+  $smtpd_tls_cert_file             = undef
+  $smtpd_tls_key_file              = undef
+  $smtpd_tls_loglevel              = 0
+  $smtpd_tls_received_header       = true
+  $smtpd_tls_session_cache_timeout = '3600s'
+
+  $biff                            = false
+  $append_dot_domain               = false
+  $mailbox_size_limit              = 0
+  $message_size_limit              = 10240000 # 10mb
   case $::osfamily {
     FreeBSD: {
       $service_restart    = '/usr/sbin/service postfix reload'
@@ -43,22 +60,34 @@ class postfix::params {
       $manpage_directory  = '/usr/local/man'
       $sample_directory   = '/usr/local/etc/postfix'
       $readme_directory   = '/usr/local/share/doc/postfix'
+
+      $smtp_tls_CApath    = undef
+      $smtp_tls_CAfile    = '/usr/local/share/certs/ca-root-nss.crt'
+      $smtpd_tls_CApath   = undef
+      $smtpd_tls_CAfile   = '/usr/local/share/certs/ca-root-nss.crt'
+
+      $tls_random_source  = 'dev:/dev/urandom'
     }
     default: {
-      $service_restart    = "/etc/init.d/postfix reload"
-
-      # Not sure what these would be for linux
-      $data_directory     = '/var/db/postfix'
+      $service_restart    = '/etc/init.d/postfix reload'
+      $data_directory     = '/var/lib/postfix'
       $queue_directory    = '/var/spool/postfix'
-      $command_directory  = '/usr/local/sbin'
-      $daemon_directory   = '/usr/local/libexec/postfix'
-      $mailq_path         = '/usr/local/bin/mailq'
-      $sendmail_path      = '/usr/local/sbin/sendmail'
-      $newaliases_path    = '/usr/local/bin/newaliases'
-      $html_directory     = '/usr/local/share/doc/postfix'
-      $manpage_directory  = '/usr/local/man'
-      $sample_directory   = '/usr/local/etc/postfix'
-      $readme_directory   = '/usr/local/share/doc/postfix'
+      $command_directory  = '/usr/sbin'
+      $daemon_directory   = '/usr/lib/postfix'
+      $mailq_path         = '/usr/bin/mailq'
+      $sendmail_path      = '/usr/sbin/sendmail'
+      $newaliases_path    = '/usr/bin/newaliases'
+      $html_directory     = 'no'
+      $manpage_directory  = '/usr/share/man'
+      $sample_directory   = '/usr/share/doc/postfix/examples'
+      $readme_directory   = '/usr/share/doc/postfix'
+
+      $smtp_tls_CApath    = '/etc/ssl/certs'
+      $smtp_tls_CAfile    = undef
+      $smtpd_tls_CApath   = '/etc/ssl/certs'
+      $smtpd_tls_CAfile   = undef
+
+      $tls_random_source  = 'dev:/dev/urandom'
     }
   }
 }
